8697ff728a21a2f25bb39a52191fa6e07d752d98
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-02-20 00:36:45 -0800

    Initial Commit

---
+++ Habitual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Bo on 2/20/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
