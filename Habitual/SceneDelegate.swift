e13930a900d26b846632d7e0b0589daa0e3d72d2
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-02-20 15:42:42 -0800

    remove storyboard, set up first view controller

--- Habitual/SceneDelegate.swift
+++ Habitual/SceneDelegate.swift
@@ -17,7 +17,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Create and set the window to be the same size as the screen
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // Create an instance of the main view controller
+        let mainController = UIViewController()
+        mainController.view.backgroundColor = .green
+
+        // Tell the window to load the main controller as it's root view
+        window?.rootViewController = mainController
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
