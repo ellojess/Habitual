397ef92abc44ed661c2c11fecf172d42e75a77a2
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-03 00:05:15 -0800

    implement persisting habits

--- Habitual/Views/PersistenceLayer.swift
+++ Habitual/Views/PersistenceLayer.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+
 struct PersistenceLayer {
 
     // Step 1
@@ -19,33 +20,98 @@ struct PersistenceLayer {
         // Step 3
         self.loadHabits()
     }
-}
-
-// Step 4
-private mutating func loadHabits() {
-
-    // Step 5
-    let userDefaults = UserDefaults.standard
-
-    // Step 6
-    guard
-        let habitData = userDefaults.data(forKey: PersistenceLayer.userDefaultsHabitsKeyValue),
-        let habits = try? JSONDecoder().decode([Habit].self, from: habitData) else {
-            return
+    
+    // Step 4
+    private mutating func loadHabits(){
+        // Step 5
+        let userDefaults = UserDefaults.standard
+        // Step6
+        guard
+            let habitData = userDefaults.data(forKey: PersistenceLayer.userDefaultsHabitsKeyValue),
+            let habits = try? JSONDecoder().decode([Habit].self, from: habitData) else {
+                return
+        }
+        self.habits = habits
     }
+    
+     // Step 7
+    @discardableResult
 
-    self.habits = habits
+       // Step 8
+       mutating func createNewHabit(name: String, image: Habit.Images) -> Habit {
+
+           let newHabit = Habit(title: name, image: image)
+           self.habits.insert(newHabit, at: 0) // Prepend the habits to the array
+           self.saveHabits()
+
+           return newHabit
+       }
+    
+    private func saveHabits(){
+        //step 9
+        guard let habitsData = try? JSONEncoder().encode(self.habits)else{
+            fatalError("could not encode lits of habits")
+        }
+        
+        //Step 10
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(habitsData, forKey: PersistenceLayer.userDefaultsHabitsKeyValue)
+        userDefaults.synchronize()
+    }
+    
+    // Step 11
+    mutating func delete(_ habitIndex: Int) {
+        // Remove habit at the given index
+        self.habits.remove(at: habitIndex)
+
+        // Persist the changes we made to our habits array
+        self.saveHabits()
+    }
+    
+    mutating func markHabitAsCompleted(_ habitIndex: Int) -> Habit {
+
+       // Step 12
+        var updatedHabit = self.habits[habitIndex]
+
+       // Step 13
+        guard updatedHabit.completedToday == false else { return updatedHabit }
+
+        updatedHabit.numberOfCompletions += 1
+
+        // Step 14
+        if let lastCompletionDate = updatedHabit.lastCompletionDate, lastCompletionDate.isYesterday {
+            updatedHabit.currentStreak += 1
+        } else {
+            updatedHabit.currentStreak = 1
+        }
+
+        // Step 15
+        if updatedHabit.currentStreak > updatedHabit.bestStreak {
+            updatedHabit.bestStreak = updatedHabit.currentStreak
+        }
+
+        // Step 16
+        let now = Date()
+        updatedHabit.lastCompletionDate = now
+
+        // Step 17
+        self.habits[habitIndex] = updatedHabit
+
+        // Step 18
+        self.saveHabits()
+        return updatedHabit
+    }
+    
+    //step 19
+    mutating func swapHabits(habitIndex: Int, destinationIndex: Int){
+        let habitToSwap = self.habits[habitIndex]
+        self.habits.remove(at: habitIndex)
+        self.habits.insert(habitToSwap, at: destinationIndex)
+        self.saveHabits()
+    }
+    //step 20
+    mutating func setNeedsToReloadHabits(){
+        self.loadHabits()
+    }
 }
 
-// Step 7
-@discardableResult
-
-   // Step 8
-   mutating func createNewHabit(name: String, image: Habit.Images) -> Habit {
-
-       let newHabit = Habit(title: name, image: image)
-       self.habits.insert(newHabit, at: 0) // Prepend the habits to the array
-       self.saveHabits()
-
-       return newHabit
-   }
