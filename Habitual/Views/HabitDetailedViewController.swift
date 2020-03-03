4f0933dbfa343433061a05ce8bdaf68ec14dfa87
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-02 23:31:13 -0800

    implement views

---
+++ Habitual/Views/HabitDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HabitDetailedViewController.swift
+//  Habitual
+//
+//  Created by Bo on 3/2/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class HabitDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
