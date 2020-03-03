cbb30a9ec2742860bb69979076d1f793aead5949
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-03 00:03:41 -0800

    create habit model

--- Habitual/Views/Habit.swift
+++ Habitual/Views/Habit.swift
@@ -7,8 +7,52 @@
 //
 
 import Foundation
+import UIKit
 
-struct Habit {
-    var title: String
+struct Habit: Codable {
+    enum Images: Int, Codable, CaseIterable {
+        case book
+        case bulb
+        case clock
+        case code
+        case drop
+        case food
+        case grow
+        case gym
+        case heart
+        case lotus
+        case other
+        case pet
+        case pill
+        case search
+        case sleep
+        case tooth
+
+        var image: UIImage {
+            guard let image = UIImage(named: String(describing: self)) else {
+                fatalError("image \(self) not found")
+            }
+
+            return image
+        }
+    }
+    
+      var title: String
+    let dateCreated: Date = Date()
+    var selectedImage: Habit.Images
+
+    var currentStreak: Int = 0
+    var bestStreak: Int = 0
+    var lastCompletionDate: Date?
+    var numberOfCompletions: Int = 0
+
+    var completedToday: Bool {
+        return lastCompletionDate?.isToday ?? false
+    }
+
+    init(title: String, image: Habit.Images) {
+        self.title = title
+        self.selectedImage = image
+    }
     
 }
