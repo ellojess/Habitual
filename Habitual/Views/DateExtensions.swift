02dced74d96750310b63ad33ff3672e1589f760f
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-03 00:01:34 -0800

    create date extensions

---
+++ Habitual/Views/DateExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  DateExtensions.swift
+//  Habitual
+//
+//  Created by Bo on 3/2/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    var stringValue: String {
+        return DateFormatter.localizedString(from: self, dateStyle: .medium, timeStyle: .none)
+    }
+
+    var isToday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInToday(self)
+    }
+
+    var isYesterday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInYesterday(self)
+    }
+}
